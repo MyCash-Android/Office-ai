6a0ff69bd5bcda06e76b83717a86ce8c8b1ba20d
RamaHad1 <ramamanafhadaki@gmail.com>
2025-02-26 21:41:37 +0300

    remove piping

--- streamer.py
+++ streamer.py
@@ -5,45 +5,7 @@ import numpy as np
 from app import process_frame
 
 rtsp_url = "rtsp://admin:Mmmycash@6699@mycash.ddns.net:56100?tcp"
-#rtsp_url = "testVid.mp4"
 
-ffmpeg_cmd = [
-    "ffmpeg",
-    "-y",
-    "-re",
-    "-rtsp_transport",
-    "tcp",
-    "-i",
-    rtsp_url,
-    "-rtbufsize",
-    "400M",
-    "-vf",
-    "scale=1280:720",
-    "-r",
-    "10",
-    "-c:v",
-    "libx264",
-    "-preset",
-    "ultrafast",
-    "-tune",
-    "zerolatency",
-    "-b:v",
-    "1000k",
-    "-f",
-    "flv",
-    "rtmp://localhost/live/office",
-    "-f",
-    "hls",
-    "-hls_time",
-    "2",
-    "-hls_list_size",
-    "10",
-    "-hls_flags",
-    "delete_segments",
-    "/var/www/html/hls/office.m3u8",
-]
-
-ffmpeg_process = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE)
 def generate():
     cap = cv2.VideoCapture(rtsp_url)
     if not cap.isOpened():
