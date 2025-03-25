aced99c89f25c4b94a1ac92a45a78f4b107d0a13
RamaHad1 <ramamanafhadaki@gmail.com>
2025-03-25 15:18:54 +0300

    use threading for decoding error

--- streamerOffice.py
+++ streamerOffice.py
@@ -1,12 +1,36 @@
+import queue
 import cv2
 import subprocess
 import time
 import numpy as np
+import threading
 from app import process_frame
 
-#rtsp_url = "rtsp://admin:Mmmycash@6699@mycash.ddns.net:56100?tcp"
-rtsp_url = "testVid.mp4"
+q=queue.Queue()
 
+rtsp_url = "rtsp://admin:Mmmycash@6699@mycash.ddns.net:56100?tcp"
+#rtsp_url = "testVid.mp4"
+
+def receive():
+    print("Start recieve")
+    cap = cv2.VideoCapture(rtsp_url)
+    ret, frame = cap.read()
+    q.put(frame)
+    while ret:
+        ret, frame = cap.read()
+        q.put(frame)
+
+def generate():
+    frame_count = 0
+    frame_skip = 1
+    print("Start generate")
+    while True:
+        if q.empty() != True:
+            frame=q.get()
+            if frame_count % frame_skip == 0:
+                process_frame(frame, frame_count=frame_count, frame_skip=frame_skip)
+                frame_count += 1
+"""
 def generate():
     cap = cv2.VideoCapture(rtsp_url)
     if not cap.isOpened():
@@ -52,11 +76,14 @@ def generate():
         prev_frame = masked_gray_frame.copy() 
         frame_count += 1
         time.sleep(0.05)
-
+"""
 
 if __name__ == "__main__":
     try:
-        generate()
+        p1=threading.Thread(target=receive)
+        p2=threading.Thread(target=generate)
+        p1.start()
+        p2.start()
     except KeyboardInterrupt:
         print("Streaming interrupted by user.")
     except Exception as e:
